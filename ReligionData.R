e140156953d6f81458f1f3668a33544dde683c77
yuya737 <kawakami@grinnell.edu>
2018-04-05 13:28:13 -0500

    Adding App.R Final

--- ReligionData.R
+++ ReligionData.R
@@ -6,19 +6,20 @@ religionData <- read.csv("religion.csv")
 
 ui <- fluidPage(
   titlePanel("World Religion Trend from 1945 - 2010"),
-  h5(em("Data From The World Religion Dataset, 1945 to 2010: Logic, Estimates, and Trends")),
+  h5(em("Data From Zeev Maoz and Errol A. Henderdon, The World Religion Dataset, 1945 to 2010: Logic, Estimates, and Trends")),
   sidebarLayout(
     sidebarPanel(
-      helpText("Please select a Region, Religion and a Year Range"),
-      
-      selectInput("Region", 
-                  label = "Choose a Region",
-                  choices = c("West Hemisphere", 
-                              "Asia-Pacific",
-                              "Africa", 
-                              "Middle East",
-                              "Europe"),
-                  selected = "West Hemisphere"),
+      helpText("Please select Region(s), Religion(s) and a Year Range"),
+      checkboxGroupInput("Region", 
+                         label = "Choose Region(s)",
+                         
+                         choices = list("Africa" = "Africa",
+                                        "Asia-Pacific" = "Asia-Pacific", 
+                                        "Europe" = "Europe",
+                                        "Middle East" = "Middle East",
+                                        "West Hemisphere" = "West Hemisphere"
+                                        ),
+                         selected = "Africa"),
       checkboxGroupInput("Religion", 
                          label = "Choose Religion(s)",
                         
@@ -29,7 +30,7 @@ ui <- fluidPage(
                                         "Hinduism" = "Hinduism",
                                         "Other religion" = "Other religion",
                                         "No religion" = "No religion"),
-                         selected = "Christianity"),
+                         selected = "Christianity"), 
       
       sliderInput("range", 
                   label = "Year Range:",
@@ -37,9 +38,6 @@ ui <- fluidPage(
     ),
     
     mainPanel(
-      textOutput("selected_Region"),
-      textOutput("selected_Religion"),
-      textOutput("selected_range"),
       textOutput("title"),
       tags$head(tags$style("#title{color: Black;
                                  font-size: 20px;
@@ -47,8 +45,7 @@ ui <- fluidPage(
       )
       )
     ,
-      
-      plotOutput("plot")
+      plotOutput("plot", height = 630 )
     )
   )
 )
@@ -56,32 +53,158 @@ ui <- fluidPage(
 
 server <- function(input, output){
   
-  output$plot <- renderPlot({
-    Asia <- subset(testReligion, region == "Asia")
-    WestH <- subset(testReligion, region == "West. Hem")
-    Africa <- subset(testReligion, region == "Africa")
-    MidE <- subset(testReligion, region == "Mideast")
-    Europe <- subset(testReligion, region == "Europe")
-    if (input$Region == "West Hemisphere"){
-      selectedRegion <- WestH
-    } else if (input$Region == "Asia-Pacific"){
-      selectedRegion <- Asia
-    } else if (input$Region == "Africa") {
-      selectedRegion <- Africa
-    } else if (input$Region == "Middle East"){
-      selectedRegion <- MidE
+  returnReligion <- function(x) {
+    if (x == "Christianity"){
+      return ("christianity_percent")
+    } else if (x == "Judaism"){
+      return ("judaism_percent")
+    } else if (x == "Buddhism"){
+      return ("buddhism_percent")
+    } else if (x == "Islam"){
+      return ("islam_percent")
+    } else if (x == "Other religion"){
+      return ("otherreligion_percent")
+    } else if (x == "Hinduism"){
+      return ("hinduism_percent")  
     } else {
-      selectedRegion <- Europe
+      return ("noreligion_percent")
     }
-    temp <- select(selectedRegion, c(year, christianity_percent, judaism_percent, buddhism_percent, islam_percent, noreligion_percent, otherreligion_percent))
-    p <- ggplot(data = temp) + geom_line(aes(x = temp$year, y = temp$christianity_percent)) + ylim(0,1)
-    p <- p + xlim(input$range[1], input$range[2]) + xlab("Year") + ylab(paste("Percentage of Selected Religion(s) in ", input$Region))
-    p
+  }
   
+  regionlist <- c(
+    'Africa'="Africa",
+    'Europe'="Europe",
+    'Mideast'="Middle East",
+    'West. Hem'="West Hemisphere",
+    'Asia' = "Asia-Pacific"
+  )
+
+  
+  
+  
+  
+  output$plot <- renderPlot({
+    Asia <- subset(religionData, region == "Asia")
+    WestH <- subset(religionData, region == "West. Hem")
+    Africa <- subset(religionData, region == "Africa")
+    MidE <- subset(religionData, region == "Mideast")
+    Europe <- subset(religionData, region == "Europe")
+    regionLen <- length(input$Region)
+    firstRegion <- input$Region[1]
+    if (firstRegion == "West Hemisphere"){
+      regionFinal <- WestH
+    }
+    else if (firstRegion == "Asia-Pacific"){
+      regionFinal <- Asia
+    }
+    else if (firstRegion == "Africa"){
+      regionFinal <- Africa
+    } 
+    else if (firstRegion == "Middle East"){
+      regionFinal <- MidE
+    }
+    else {
+      regionFinal <- Europe
+    }
+    if (regionLen > 1){
+      for (i in 2 : regionLen){
+        if (input$Region[i] == "West Hemisphere"){
+          regionFinal <- rbind(regionFinal, WestH)
+        }
+        else if (input$Region[i] == "Asia-Pacific"){
+          regionFinal <- rbind(regionFinal, Asia)
+        }
+        else if (input$Region[i] == "Africa"){
+          regionFinal <- rbind(regionFinal, Africa)
+        } 
+        else if (input$Region[i] == "Middle East"){
+          regionFinal <- rbind(regionFinal, MidE)
+        }
+        else {
+          regionFinal <- rbind(regionFinal, Europe)
+        }
+      }
+    }
+    # if (input$Region == "West Hemisphere"){
+    #   selectedRegion <- WestH
+    # } else if (input$Region == "Asia-Pacific"){
+    #   selectedRegion <- Asia
+    # } else if (input$Region == "Africa") {
+    #   selectedRegion <- Africa
+    # } else if (input$Region == "Middle East"){
+    #   selectedRegion <- MidE
+    # } else {
+    #   selectedRegion <- Europe
+    # }
+    len <- length(input$Religion)
+    religionVec <- vector()
+    if (len != 0){
+      religionVec <- c(religionVec, returnReligion(input$Religion[1]))
+      if (len > 1){
+        for (i in 2:len){
+          religionVec <- c(religionVec, returnReligion(input$Religion[i]))
+        }
+      }
+    }
+    selected_data <- select(regionFinal, c("region", religionVec, "year"))
+    #print(dim(selected_data))
+    #print(head(selected_data))
+    p <- ggplot(data = selected_data, aes(x = selected_data$year))+ ylim(0,1) +  scale_color_discrete(name  ="Religion") + theme(legend.text=element_text(size=15)) + theme(legend.title=element_text(size=16))
+    p <- p + xlim(input$range[1], input$range[2]) + xlab("Year") + ylab(paste("Percentage of Selected Religion(s) in Selected Region(s)")) + theme(axis.text=element_text(size=15), axis.title=element_text(size=15)) 
+    for (i in 1:len){
+      if (religionVec[i] == "christianity_percent"){
+        p <- p + geom_line(size = 1, aes(y = selected_data$christianity_percent, color = "Christianity")) + geom_point(aes(y = selected_data$christianity_percent, color = "Christianity"), size = 2)
+      } else if (religionVec[i] == "judaism_percent"){
+        p <- p + geom_line(size = 1, aes(y = selected_data$judaism_percent, color = "Judaism")) + geom_point(aes(y = selected_data$judaism_percent, color = "Judaism"), size = 2)
+      } else if (religionVec[i] == "hinduism_percent"){
+        p <- p + geom_line(size = 1, aes(y = selected_data$hinduism_percent, color = "Hinduism"))+ geom_point(aes(y = selected_data$hinduism_percent, color = "Hinduism"), size = 2)
+      } else if (religionVec[i] == "buddhism_percent"){
+        p <- p + geom_line(size = 1, aes(y = selected_data$buddhism_percent, color = "Buddhism"))+ geom_point(aes(y = selected_data$buddhism_percent, color = "Buddhism"), size = 2)
+      } else if (religionVec[i] == "islam_percent"){
+        p <- p + geom_line(size = 1, aes(y = selected_data$islam_percent, color = "Islam"))+ geom_point(aes(y = selected_data$islam_percent, color = "Islam"), size = 2)
+      } else if (religionVec[i] == "otherreligion_percent"){
+        p <- p + geom_line(size = 1, aes(y = selected_data$otherreligion_percent, color = "Other Religion"))+ geom_point(aes(y = selected_data$otherreligion_percent,color = "Other Religion"), size = 2)
+      } else {
+        p <- p  + geom_line(size = 1, aes(y = selected_data$noreligion_percent,color = "No Religion"))+ geom_point(aes(y = selected_data$noreligion_percent, color = "No Religion"), size = 2)
+      }
+    }
+    p <- p + facet_grid( ~ region , labeller = as_labeller(regionlist))+ theme(strip.text.x = element_text(size = 15))
+   
+    p
   })
   
   output$title <- renderText({
-    paste(c("Percentage of ", input$Religion , "in ", input$Region))
+    strOut <- input$Religion[1]
+    len <- length(input$Religion)
+    if (len > 1){
+      if (len == 2){
+        strOut <- paste(strOut, paste("and ", input$Religion[2]))
+      } else {
+        for (i in 2:len){
+          if (i != len){
+            strOut <- paste(strOut, paste("," , input$Religion[i]))
+          } else{
+            strOut <- paste(strOut, paste(", and", input$Religion[i]))
+          }
+        }
+      }
+    }
+    len2 <- length(input$Region)
+    strOut2 <- input$Region[1]
+    if (len2 > 1){
+      if (len2 == 2){
+        strOut2 <- paste(strOut2, paste("and ", input$Region[2]))
+      } else {
+        for (i in 2:len2){
+          if (i != len2){
+            strOut2 <- paste(strOut2, paste("," , input$Region[i]))
+          } else{
+            strOut2 <- paste(strOut2, paste(", and", input$Region[i]))
+          }
+        }
+      }
+    }
+    paste("Percentage of ", strOut , "in ", strOut2)
   })
   
   output$selected_Region <- renderText({ 
